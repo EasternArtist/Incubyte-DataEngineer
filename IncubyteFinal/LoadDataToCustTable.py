a36e43970849078761b17486ac9ad00ddb30cbaf
Dhruv <52623929+EasternArtist@users.noreply.github.com>
2021-09-02 22:55:53 +0530

    Update LoadDataToCustTable.py

--- IncubyteFinal/LoadDataToCustTable.py
+++ IncubyteFinal/LoadDataToCustTable.py
@@ -1,24 +1,53 @@
 import pandas as pd
 import sqlite3
+from datetime import datetime
 
 filename = "CustomerDetails.dsv"
 
+
 data = pd.read_csv(filename,sep="|",skiprows = 1,header = None,usecols=range(2,12))
 
-#print(data)
+data.columns = ['name','cust_id','open_date','consult_date','vac_type','dr_name','state','country',
+                'dob','cust_status']
 
-connection = sqlite3.connect('HospitalRecords.db')
+try:    
+    for i in range(len(data)) :
+        row = data.loc[i, "open_date"] 
+        row  = datetime.strptime(str(row),'%Y%m%d').date()
+        data.loc[i,'open_date'] = row
+    
+    for i in range(len(data)) :
+        row = data.loc[i, "consult_date"] 
+        row  = datetime.strptime(str(row),'%Y%m%d').date()
+        data.loc[i,'consult_date'] = row
+    
+    for i in range(len(data)) :
+        row = data.loc[i, "dob"] 
+        row  = datetime.strptime(str(row),'%d%m%Y').date()
+        data.loc[i,'dob'] = row
 
-c = connection.cursor()
+except ValueError:
+    pass
 
-data.to_sql('cust_details', connection, if_exists='replace', index = False)
+    
+try:   
+    connection = sqlite3.connect('HospitalRecords.db')
+    
+    c = connection.cursor()
+    
+    
+    data.to_sql('cust_details', connection, if_exists='replace', index = False)
+    
+    
+    c.execute('''
+    SELECT * FROM cust_details
+              ''')
+    
+    for row in c.fetchall():
+        print (row)
 
+except sqlite3.Error as error:
+    print("Failed to load in table ", error)
 
-c.execute('''
-SELECT * FROM cust_details
-          ''')
-
-for row in c.fetchall():
-    print (row)
-
-c.close()
+finally:   
+    c.close()
